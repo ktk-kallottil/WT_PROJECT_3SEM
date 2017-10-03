3c60d35ce84493239527a9c25d2cc4ae421bcdba
kiran s hombal <kiranhombal98@gmail.com>
2017-10-04 00:19:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,119 @@
+function image()
+{
+  var i_1 = document.getElementById('img1');
+  i_1.setAttribute("src","image/1.jpg");
+  var i_2 = document.getElementById('img2');
+  i_2.setAttribute("src","image/2.jpg");
+  var i_4 = document.getElementById('img4');
+  i_4.setAttribute("src","image/4.jpg");
+  var i_6 = document.getElementById('img6');
+  i_6.setAttribute("src","image/6.jpg");
+  var i_7 = document.getElementById('img7');
+  i_7.setAttribute("src","image/7.jpg");
+  var i_9 = document.getElementById('img9');
+  i_9.setAttribute("src","image/9.jpg");
+  var i_11 = document.getElementById('img11');
+  i_11.setAttribute("src","image/11.jpg");
+  var i_12 = document.getElementById('img12');
+  i_12.setAttribute("src","image/12.jpg");
+  var i_14 = document.getElementById('img14');
+  i_14.setAttribute("src","image/14.jpg");
+  var i_15 = document.getElementById('img15');
+  i_15.setAttribute("src","image/15.jpg");
+  var i_3 = document.getElementById('img3');
+  i_3.setAttribute("src","image/3.jpg");
+  var i_5 = document.getElementById('img5');
+  i_5.setAttribute("src","image/5.jpg");
+  var i_10 = document.getElementById('img10');
+  i_10.setAttribute("src","image/10.jpg");
+  var i_8 = document.getElementById('img8');
+  i_8.setAttribute("src","image/8.jpg");
+  var i_13 = document.getElementById('img13');
+  i_13.setAttribute("src","image/13.jpg");
+  var i_16 = document.getElementById('img16');
+  i_16.setAttribute("src","image/16.jpg");
+}
+function image1a()
+{
+    var i_1 = document.getElementById('img1');
+    i_1.setAttribute("src","image/16.jpg");
+    var i_3 = document.getElementById('img3');
+    i_3.setAttribute("src","image/15.jpg");
+    var i_5 = document.getElementById('img5');
+    i_5.setAttribute("src","image/14.jpg");
+    var i_7 = document.getElementById('img7');
+    i_7.setAttribute("src","image/13.jpg");
+    var i_9 = document.getElementById('img9');
+    i_9.setAttribute("src","image/12.jpg");
+    var i_11 = document.getElementById('img11');
+    i_11.setAttribute("src","image/11.jpg");
+    var i_13 = document.getElementById('img13');
+    i_13.setAttribute("src","image/10.jpg");
+    var i_15 = document.getElementById('img15');
+    i_15.setAttribute("src","image/9.jpg");
+
+}
+function image1b()
+{
+  var i_2 = document.getElementById('img2');
+  i_2.setAttribute("src","image/1.jpg");
+  var i_4 = document.getElementById('img4');
+  i_4.setAttribute("src","image/2.jpg");
+  var i_6 = document.getElementById('img6');
+  i_6.setAttribute("src","image/3.jpg");
+  var i_8 = document.getElementById('img8');
+  i_8.setAttribute("src","image/4.jpg");
+  var i_10 = document.getElementById('img10');
+  i_10.setAttribute("src","image/19.jpg");
+  var i_12 = document.getElementById('img12');
+  i_12.setAttribute("src","image/6.jpg");
+  var i_14 = document.getElementById('img14');
+  i_14.setAttribute("src","image/7.jpg");
+  var i_16 = document.getElementById('img16');
+  i_16.setAttribute("src","image/8.jpg");
+}
+function image2a()
+{
+  var i_1 = document.getElementById('img1');
+  i_1.setAttribute("src","image/9.jpg");
+  var i_2 = document.getElementById('img2');
+  i_2.setAttribute("src","image/10.jpg");
+  var i_4 = document.getElementById('img4');
+  i_4.setAttribute("src","image/11.jpg");
+  var i_6 = document.getElementById('img6');
+  i_6.setAttribute("src","image/16.jpg");
+  var i_7 = document.getElementById('img7');
+  i_7.setAttribute("src","image/13.jpg");
+  var i_9 = document.getElementById('img9');
+  i_9.setAttribute("src","image/17.jpg");
+  var i_11 = document.getElementById('img11');
+  i_11.setAttribute("src","image/15.jpg");
+  var i_12 = document.getElementById('img12');
+  i_12.setAttribute("src","image/12.jpg");
+  var i_14 = document.getElementById('img14');
+  i_14.setAttribute("src","image/1.jpg");
+  var i_15 = document.getElementById('img15');
+  i_15.setAttribute("src","image/2.jpg");
+
+}
+function image2b()
+{
+  var i_3 = document.getElementById('img3');
+  i_3.setAttribute("src","image/3.jpg");
+  var i_5 = document.getElementById('img5');
+  i_5.setAttribute("src","image/4.jpg");
+  var i_10 = document.getElementById('img10');
+  i_10.setAttribute("src","image/18.jpg");
+  var i_8 = document.getElementById('img8');
+  i_8.setAttribute("src","image/6.jpg");
+  var i_13 = document.getElementById('img13');
+  i_13.setAttribute("src","image/22.jpg");
+  var i_16 = document.getElementById('img16');
+  i_16.setAttribute("src","image/8.jpg");
+}
+
+setInterval(image1a,3000);
+setInterval(image1b,5000);
+setInterval(image2a,7000);
+setInterval(image2b,9000);
+setInterval(image,11000);
